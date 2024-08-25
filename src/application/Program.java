c1a46d7f39c2a6d464c63ab7086fa73ef241b21b
Mateus Souza <mateusjose1903@gmail.com>
2024-08-24 23:02:35 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,13 @@ public class Program {
 				if(captured != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for promotion (B/N/R/Q)");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
